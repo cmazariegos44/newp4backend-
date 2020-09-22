eac6918760601dfbebdfececd4b242c3d67e39cb
Claudia <claudias.mazariegos@gmail.com>
2020-09-21 22:22:34 -0400

    changes

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -4,13 +4,15 @@ from rest_framework import serializers
 
 
 class ReminderSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Reminder
-        fields = ['title', 'created', 'id']
+        fields = ['title', 'created', 'id', 'owner']
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     reminder = ReminderSerializer(many=True, read_only=True, required=False)
     class Meta:
         model = Task
-        fields = ['title', 'complete', 'created', 'id', 'reminder']
+        fields = ['title', 'complete', 'created', 'id', 'reminder', 'owner']
         
